2c9eda5c3dfc4320d78ec8e3dd1e83cc996ba1a7
Isabel Aparicio <iaparicio695@gmail.com>
2024-06-17 12:50:31 -0400

    Created function for Where Is This Service?

--- scripts/serviceList.js
+++ scripts/serviceList.js
@@ -1,4 +1,58 @@
-import { getServices } from './database.js';
+import { getServices, getParkAreas } from './database.js';
+
+const services = getServices();
+const parkAreas = getParkAreas();
+
+const findAreasMatch = (serviceObject, parkAreasArray) => {
+
+  const matchingParkAreas = []
+
+  for (const park of parkAreasArray) {
+     if (park.serviceIds.includes(serviceObject.id)) {
+      matchingParkAreas.push(park)
+     }
+
+     return matchingParkAreas
+  }
+}
+
+document.addEventListener(
+  "click",
+  (theClickEvent) => {
+
+    const itemClickedOn = theClickEvent.target;
+
+    if (itemClickedOn.dataset.type === "service") {
+      const serviceId = parseInt(itemClickedOn.dataset.id)
+
+      const service = services.find(s => s.id === serviceId)
+      const serviceLocation = service.name
+
+      const areasServices = findAreasMatch(service, parkAreas)
+
+      const servicesAtPark = []
+      for (const park of parkAreas) {
+        for (const parkId of park.serviceIds)
+          if( parkId === serviceId ) {
+          servicesAtPark.push(park.name)
+        }
+      }
+   
+        let message = `${serviceLocation} is available in: `
+        if (areasServices.length === 0) {
+          message += "no park areas."
+        } else {
+          message += servicesAtPark.join(", ")
+        }
+        window.alert(message)
+    }
+
+
+  }
+  
+
+)
+
 
 export const serviceList = () => {
   const services = getServices();
